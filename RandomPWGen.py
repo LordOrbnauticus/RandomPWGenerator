ae8def18de21dc3cb263ccbb037ed445382289c9
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-09-05 02:31:44 -0500

    added .gitignore, RandomPRG file, and desktop

---
+++ RandomPWGen.py
@@ -0,0 +1,14 @@
+import random
+
+chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(-_=+)'
+password = ''
+
+print('Password Generator')
+print('==================')
+length = input("Enter your password length: ")
+length = int(length)
+
+for i in range(length):
+    password += random.SystemRandom().choice(chars)
+print(password)
+
